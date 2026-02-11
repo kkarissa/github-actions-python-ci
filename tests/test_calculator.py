e814b6097fc3b0e8a75c6557387af9297007d681
kkarissa <karissanicole0311@gmail.com>
2026-02-11 12:35:29 -0700

    created calc functions and tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,23 @@
+import unittest
+from src.calculator import add, subtract, divide 
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_subtract(self):
+        # COMPLETE HERE
+        self.assertEqual(subtract(2, 3), -1)
+        self.assertEqual(subtract(-1, 1), -2)
+        self.assertEqual(subtract(-1, -1), 0)
+
+    def test_divide(self):
+        # COMPLETE HERE
+        self.assertEqual(divide(2, 4), 0.5)
+        self.assertEqual(divide(-1, 1), -1)
+        self.assertEqual(divide(-1, 0), "Error: Division by zero")
+
+if __name__ == '__main__':
+   unittest.main()
